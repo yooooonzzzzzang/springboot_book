237669c6f9b7e771958015716bda8bcce55e1ebc
yooooonzzzzzang <cherishvalue@naver.com>
2023-01-16 08:59:35 +0900

    0116

---
+++ Workspace/board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @GetMapping("/board/write")
+   public String boardWriteForm(){
+
+       return "boardwrite";
+   }
+   @PostMapping("/board/writepro")
+   public String boardWirtePro(String title, String content){
+       System.out.println("제목: " + title);
+       System.out.println("내용: " + content);
+       return "";
+   }
+}
